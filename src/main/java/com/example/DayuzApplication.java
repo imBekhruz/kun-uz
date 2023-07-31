9af3b4c8d56d123d417f5e0f1f2c6ae49063c882
imBekhruz <@bekhruzarifov>
2023-07-31 16:13:47 +0500

    Initial commit

---
+++ src/main/java/com/example/DayuzApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayuzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DayuzApplication.class, args);
+	}
+
+}
